da672ec34166b42f212052ea8e6654026d321c71
Palash Bauri <palashbauri1@gmail.com>
2022-12-15 15:02:16 +0530

    removed webshare api + prettier

--- src/instance_search.ts
+++ src/instance_search.ts
@@ -1,26 +1,23 @@
-import {IL} from "./instance_lists"
+import { IL } from "./instance_lists";
 
-export interface MInstance{
-  name:string;
-  url:string;
-  description:string;
-  users : number;
+export interface MInstance {
+  name: string;
+  url: string;
+  description: string;
+  users: number;
 }
 
 const elm = document.querySelector<HTMLDivElement>("#instance_card")!;
-function getapi(url : string) {
-
-return `https://${url}/api/v2/instance`
-
-
+function getapi(url: string) {
+  return `https://${url}/api/v2/instance`;
 }
 
-function createInstanceCard(data : MInstance){
+function createInstanceCard(data: MInstance) {
   elm.style.visibility = "visible";
 
   const instance_title = data.name;
   const instance_desc = data.description;
-  const instance_users = data.users
+  const instance_users = data.users;
 
   elm.innerHTML = `
     
@@ -28,51 +25,45 @@ function createInstanceCard(data : MInstance){
     <p class="instance-desc">${instance_desc}</p>
     <p class="instance-users">Monthly Active Users: ${instance_users}</p>
   
-  `
-
+  `;
 }
 
-function invalidURL(){
-			elm.style.visibility = "visible";
-			elm.innerHTML = `<h2 class="instance-title">Probably not a Mastodon Instance!</h2>`	
-	
+export function invalidURL() {
+  elm.style.visibility = "visible";
+  elm.innerHTML = `<h2 class="instance-title">Probably not a Mastodon Instance!</h2>`;
 }
 
-function fetchData(url : string){
-  fetch(getapi(url)).then((data) => data.json())
+function fetchData(url: string) {
+  fetch(getapi(url))
+    .then((data) => data.json())
     .then((data) => {
-		if (data["title"].length < 1){
-			invalidURL()
-		return
-		}
-      const i : MInstance = {
-        name : data["title"],
-        url : data["url"],
-        description:data["description"],
-        users : data["usage"]["users"]["active_month"]
+      if (data["title"].length < 1) {
+        invalidURL();
+        return;
       }
-      createInstanceCard(i)
-    }).catch((_e) => {
-
-		invalidURL()
-	})
+      const i: MInstance = {
+        name: data["title"],
+        url: data["url"],
+        description: data["description"],
+        users: data["usage"]["users"]["active_month"],
+      };
+      createInstanceCard(i);
+    })
+    .catch((_e) => {
+      invalidURL();
+    });
 }
-
-
-
 
 export function setupSearch(element: HTMLButtonElement) {
-	const ielm = document.querySelector<HTMLInputElement>("#instance");
+  const ielm = document.querySelector<HTMLInputElement>("#instance");
 
- 	
-	let datalist_entry  = "";
+  let datalist_entry = "";
 
-	IL.forEach((item : string)=> {
-		datalist_entry += `<option value=${item}/>`	
-	})
+  IL.forEach((item: string) => {
+    datalist_entry += `<option value=${item}/>`;
+  });
 
-	document.getElementById("instance_list")!.innerHTML = datalist_entry;
-	
-  
-  element.addEventListener('click', () => fetchData(ielm!.value))
+  document.getElementById("instance_list")!.innerHTML = datalist_entry;
+
+  element.addEventListener("click", () => fetchData(ielm!.value));
 }
--- src/main.ts
+++ src/main.ts
@@ -1,62 +1,61 @@
-import './style.css'
-import { setupSearch} from './instance_search'
+import "./style.css";
+import { setupSearch } from "./instance_search";
+import { invalidURL } from "./instance_search.ts";
 
-const params = new URLSearchParams(window.location.search)
-const edit_text = 	document.querySelector<HTMLTextAreaElement>("#share_text");
+const params = new URLSearchParams(window.location.search);
+const edit_text = document.querySelector<HTMLTextAreaElement>("#share_text");
 const instance = document.querySelector<HTMLInputElement>("#instance");
 
-function getUrl(){
-	return	instance!.value;
+function getUrl() {
+  return instance!.value;
 }
 
-let share_text : string;
-let default_instace : string;
-let share_url : string;
-
-
-
+let share_text: string;
+let default_instace: string;
+let share_url: string;
 
 if (params.has("text")) {
-    share_text = params.get("text")!;
-	edit_text!.value = share_text;
+  share_text = params.get("text")!;
+  edit_text!.value = share_text;
 }
 
-if (params.has("default")){
-	default_instace = params.get("default")!;
-	instance!.value = default_instace
+if (params.has("default")) {
+  default_instace = params.get("default")!;
+  instance!.value = default_instace;
 }
 
-if (params.has("url")){
-	share_url = params.get("url")!;
-	edit_text!.value+= "\n" + share_url;
+if (params.has("link")) {
+  share_url = params.get("link")!;
+  edit_text!.value += "\n" + share_url;
 }
 
-function getShareText(){
-	return encodeURIComponent(edit_text!.value)
+function getShareText() {
+  return encodeURIComponent(edit_text!.value);
 }
 
-
-function setupWebShare(){
-	if (navigator.share != undefined){
-		const share_area = document.querySelector("#share_area");
-		share_area!.innerHTML += `<button id="share_button_webshare">Share via app</button>`
-		document.querySelector<HTMLButtonElement>("#share_button_webshare")!.onclick = async function(){
-			await navigator.share({
-				url : share_url,
-				text : getShareText(),
-				title : "_"
-			});
-		}
-	}
-	
+function isValidUrl(url: string) {
+  try {
+    return Boolean(new URL(url));
+  } catch (_) {
+    return false;
+  }
 }
 
+const share_button = document.querySelector<HTMLButtonElement>("#share_button");
+setupSearch(document.querySelector<HTMLButtonElement>("#check_button")!);
 
-setupSearch(document.querySelector<HTMLButtonElement>('#check_button')!)
-setupWebShare()
+share_button!.onclick = function () {
+  ShareURL(true);
+};
 
-document.querySelector<HTMLButtonElement>("#share_button")!.onclick = function(){
-	
-	window.open(`https://${getUrl()}/share/?text=${getShareText()}` , "_blank")
+function ShareURL(url_check: boolean) {
+  if (url_check && !isValidUrl("http://" + getUrl())) {
+    invalidURL();
+    share_button!.innerText = "Share Anyway!";
+    share_button!.onclick = function (_: Event) {
+      ShareURL(false);
+    };
+  } else {
+    window.open(`https://${getUrl()}/share/?text=${getShareText()}`, "_blank");
+  }
 }
-
